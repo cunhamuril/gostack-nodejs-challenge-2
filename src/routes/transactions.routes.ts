3296708b6375499c8a083d8494285b3d53c7b127
Murilo Cunha <murilo.sant@hotmail.com>
2020-09-02 09:43:11 -0300

    refactor(transactions): added eager loading feature on Transacation model and added cors on app

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -20,48 +20,59 @@ transactionsRouter.get('/', async (request, response) => {
 
   const balance = await transactionsRepository.getBalance();
 
-  const queryBuilder = transactionsRepository.createQueryBuilder('transaction');
-
-  const transactionsQuery = await queryBuilder
-    .select([
-      'transaction.id',
-      'transaction.title',
-      'transaction.value',
-      'transaction.type',
-      'category.*',
-      'transaction.created_at',
-      'transaction.updated_at',
-    ])
-    .innerJoin(
-      query => {
-        return query.from(Category, 'category');
-      },
-      'category',
-      'category.id = transaction.category_id',
-    )
-    .getRawMany();
-
-  const formattedTransactions = transactionsQuery.map(transaction => {
-    return {
-      id: transaction.transaction_id,
-      title: transaction.transaction_title,
-      value: transaction.transaction_value,
-      type: transaction.transaction_type,
-      category: {
-        id: transaction.id,
-        title: transaction.title,
-        created_at: transaction.created_at,
-        updated_at: transaction.updated_at,
-      },
-      created_at: transaction.transaction_created_at,
-      updated_at: transaction.transaction_updated_at,
-    };
-  });
+  const transactions = await transactionsRepository.find();
 
   return response.json({
-    transactions: formattedTransactions,
+    transactions,
     balance,
   });
+
+  /**
+   * ABAIXO A FAMOSA GAMBIARRA!!
+   */
+
+  // const queryBuilder = transactionsRepository.createQueryBuilder('transaction');
+
+  // const transactionsQuery = await queryBuilder
+  //   .select([
+  //     'transaction.id',
+  //     'transaction.title',
+  //     'transaction.value',
+  //     'transaction.type',
+  //     'category.*',
+  //     'transaction.created_at',
+  //     'transaction.updated_at',
+  //   ])
+  //   .innerJoin(
+  //     query => {
+  //       return query.from(Category, 'category');
+  //     },
+  //     'category',
+  //     'category.id = transaction.category_id',
+  //   )
+  //   .getRawMany();
+
+  // const formattedTransactions = transactionsQuery.map(transaction => {
+  //   return {
+  //     id: transaction.transaction_id,
+  //     title: transaction.transaction_title,
+  //     value: transaction.transaction_value,
+  //     type: transaction.transaction_type,
+  //     category: {
+  //       id: transaction.id,
+  //       title: transaction.title,
+  //       created_at: transaction.created_at,
+  //       updated_at: transaction.updated_at,
+  //     },
+  //     created_at: transaction.transaction_created_at,
+  //     updated_at: transaction.transaction_updated_at,
+  //   };
+  // });
+
+  // return response.json({
+  //   transactions: formattedTransactions,
+  //   balance,
+  // });
 });
 
 transactionsRouter.post('/', async (request, response) => {
