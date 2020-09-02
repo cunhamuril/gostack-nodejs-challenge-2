47418a9c6de2aa86229d30851a1475e719f819c3
Murilo Cunha <murilo.sant@hotmail.com>
2020-09-01 21:30:10 -0300

    feat(import csv): added import cvs file function

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,3 +1,4 @@
+import multer from 'multer';
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 
@@ -6,7 +7,11 @@ import Category from '../models/Category';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -84,8 +89,16 @@ transactionsRouter.delete('/:id', async (request, response) => {
   response.send();
 });
 
-// transactionsRouter.post('/import', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
+
+    const transactions = await importTransactions.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
